0be0955c649de326b341d4b1c077a997a6247197
bleutruth <bleutruth@gmail.com>
2015-05-22 00:09:08 +0900

    キーボードからの割り込み受け付け完了をPICに通知するようにした。

--- src/bootpack.h
+++ src/bootpack.h
@@ -13,6 +13,8 @@ struct BOOTINFO { /* 0x0ff0-0x0fff */
 void io_hlt(void);
 void io_cli(void);
 void io_sti(void);
+void io_stihlt(void);
+int io_in8(int port);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
@@ -73,6 +75,9 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define AR_INTGATE32	0x008e
 
 /* int.c */
+struct KEYBUF {
+	unsigned char data, flag;
+};
 void init_pic(void);
 #define PIC0_ICW1		0x0020
 #define PIC0_OCW2		0x0020
--- src/myosfunc.nas
+++ src/myosfunc.nas
@@ -4,7 +4,7 @@
 [BITS 32]						; 32ビットモード用の機械語を作らせる
 [FILE "myosfunc.nas"]			; ソースファイル名情報
 
-		GLOBAL	_io_hlt, _io_cli, _io_sti, io_stihlt
+		GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
 		GLOBAL	_io_in8,  _io_in16,  _io_in32
 		GLOBAL	_io_out8, _io_out16, _io_out32
 		GLOBAL	_io_load_eflags, _io_store_eflags
